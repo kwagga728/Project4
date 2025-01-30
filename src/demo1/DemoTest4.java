0df1db942310552331dfe09a5b8900465b354119
kwagga728 <brettvstaden@gmail.com>
2025-01-29 21:26:06 -0500

    Update DemoTest4.java
    test

--- src/demo1/DemoTest4.java
+++ src/demo1/DemoTest4.java
@@ -5,6 +5,15 @@ public class DemoTest4 {
 public static void main(String[] args) {
 		
 		System.out.println("Java");
+
+		int x = 10;
+		int y = 20;
+
+		if(x > y) {
+			System.out.println("Greater");
+		} else {
+			System.out.println("Less");
+		}
 		
 	}
 
